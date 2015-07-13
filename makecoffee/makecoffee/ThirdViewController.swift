787b2be9aff66e31914657c060df6663052f7466
admin <admin@admins-MacBook-Pro.local>
2015-07-12 18:26:40 -0700

    added three view with elements and images assets

---
+++ makecoffee/makecoffee/ThirdViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FirstViewController.swift
+//  makecoffee
+//
+//  Created by admin on 7/12/15.
+//  Copyright (c) 2015 fishorse. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    @IBOutlet weak var ouncesReqLabel: DesignableLabel!
+    
+    @IBOutlet weak var coffeeQuestionLabel: DesignableLabel!
+    
+    @IBOutlet weak var ouncesReqTextField: UITextField!
+    
+    @IBAction func coffeeReqBtn(sender: AnyObject) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
